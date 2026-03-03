dde91567ad851a54979bf5b97a526e447836c059
hanan3889 <rebaia_hanan@yahoo.fr>
2026-03-03 14:13:15 +0100

    add new routes

--- frontend/src/js/routes/two-factor.ts
+++ frontend/src/js/routes/two-factor.ts
@@ -8,3 +8,13 @@ export function qrCode(): string {
     // This should correspond to an API endpoint that returns the QR code SVG or data
     return '/user/two-factor-qr-code';
 }
+
+export function secretKey(): string {
+    // This should correspond to an API endpoint that returns the secret key
+    return '/user/two-factor-secret-key';
+}
+
+export function recoveryCodes(): string {
+    // This should correspond to an API endpoint that returns the recovery codes
+    return '/user/two-factor-recovery-codes';
+}
