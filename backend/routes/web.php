8dea79e6374362456ace61cdfd3024d54de65743
hanan3889 <rebaia_hanan@yahoo.fr>
2026-03-06 12:17:09 +0100

    add new routes in api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\PageInformation;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,7 +11,25 @@ use Inertia\Inertia;
 */
 
 // Route principale
-Route::get('/', function () { return Inertia::render('Home'); })->name('home');
+Route::get('/', function () {
+    $pages = PageInformation::with('categorie')
+        ->where('statut', 'publie')
+        ->latest()
+        ->take(5)
+        ->get();
+    return Inertia::render('Home', ['pages' => $pages]);
+})->name('home');
+
+// Information Pages
+Route::get('/informations', function () {
+    $pages = PageInformation::with('categorie')->latest()->get();
+    return Inertia::render('Information/Index', ['pages' => $pages]);
+})->name('information.index');
+
+Route::get('/informations/{page:slug}', function (PageInformation $page) {
+    $page->load('categorie');
+    return Inertia::render('Information/Show', ['page' => $page]);
+})->name('information.show');
 
 // Dashboard
 Route::get('/dashboard', function () { return Inertia::render('Dashboard'); })->name('dashboard');
@@ -32,3 +51,4 @@ Route::get('/confirm-password', function () { return response('OK'); })->name('p
 
 // Profile (legacy)
 Route::get('/user/profile', function () { return response('OK'); })->name('profile.show');
+
